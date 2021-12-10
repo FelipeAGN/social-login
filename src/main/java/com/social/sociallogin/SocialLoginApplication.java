319a2cd62e703ab7e11bc11f22d365b3bcb85eae
advanze <felipe@agile.cl>
2021-12-10 10:57:54 -0300

    test one

---
+++ src/main/java/com/social/sociallogin/SocialLoginApplication.java
@@ -0,0 +1,69 @@
+package com.social.sociallogin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Collections;
+import java.util.Map;
+
+@SpringBootApplication
+@RestController
+public class SocialLoginApplication extends WebSecurityConfigurerAdapter {
+
+    @GetMapping("/user")
+    public Map<String,Object> user(@AuthenticationPrincipal OAuth2User principal){
+        return Collections.singletonMap("name", principal.getAttribute("name"));
+    }
+
+    @GetMapping("/error")
+    @ResponseBody
+    public String error(HttpServletRequest request) {
+        String message = (String) request.getSession().getAttribute("error.message");
+        request.getSession().removeAttribute("error.message");
+        return message;
+    }
+
+    @Override
+    protected void configure(HttpSecurity httpSecurity) throws Exception{
+
+        SimpleUrlAuthenticationFailureHandler handler = new SimpleUrlAuthenticationFailureHandler("/");
+
+        httpSecurity
+                .authorizeRequests(a -> a
+                    .antMatchers("/", "/error", "/webjars/**")
+                        .permitAll()
+                        .anyRequest()
+                        .authenticated())
+                .exceptionHandling(e-> e
+                        .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
+                )
+                .csrf(c -> c
+                    .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()))
+                .logout(l -> l
+                    .logoutSuccessUrl("/").permitAll())
+                .oauth2Login(o -> o
+                    .failureHandler((request, response, exception) -> {
+                        request.getSession().setAttribute("error.message", exception.getMessage());
+                        handler.onAuthenticationFailure(request,response,exception);
+                    })
+                );
+
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(SocialLoginApplication.class, args);
+    }
+
+}
